7c8cfab95de8b253900319b17c2c0746acdc8a78
Gabriel442018 <37821374+Gabriel4420@users.noreply.github.com>
2021-01-31 16:45:17 -0300

    shorticon cut

--- pages/_app.js
+++ pages/_app.js
@@ -50,6 +50,7 @@ export default function App({ Component, pageProps }) {
       <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
+        <link rel="icon" className="js-site-favicon" type="image/svg+xml" href="https://github.githubassets.com/favicons/favicon-dark.svg" />
       </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
